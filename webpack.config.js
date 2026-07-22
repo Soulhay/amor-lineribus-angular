6f8678bab0f2157c2f1e14f4a6a6635617da29c5
Soulhay <nejcdolinar.tigri@gmail.com>
2026-07-22 22:42:26 +0200

    deploy
    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const { shareAll } = require('@angular-architects/module-federation/webpack');
 const { ModuleFederationPlugin } = require('webpack').container;
 
+/**
+ * Production federation config for the Angular remote.
+ *
+ * Identical to webpack.config.js except for publicPath, which must point at
+ * the GitHub Pages location so the remote's own lazy chunks resolve when the
+ * shell loads it from a different path.
+ */
 module.exports = {
   output: {
     uniqueName: 'amorLineribusAngular',
-    publicPath: 'http://localhost:4201/',
+    publicPath: 'https://soulhay.github.io/amor-lineribus-angular/',
     scriptType: 'text/javascript',
   },
   optimization: {
